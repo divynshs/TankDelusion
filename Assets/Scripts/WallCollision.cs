cd0876aba1aa1e2e573019e5ed54aef794d51ac6
salmantitas <salman.mehedy@gmail.com>
2018-02-12 23:38:42 -0800

    unnecessary commit

---
+++ Assets/Scripts/WallCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollision : MonoBehaviour {
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Wall")
+			Destroy (gameObject);
+	}
+}
